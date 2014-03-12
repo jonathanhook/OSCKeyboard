ec3fdb11ff666d358ff87df31db63c4a881ea6d8
Jonathan Hook <jonathan.hook@ncl.ac.uk>
2014-03-12 11:16:02 +0000

    First working version

---
+++ OSCKeyboard/OSCKeyboard/OSCKeyboard.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Bespoke.Common.Osc;
+
+namespace OSCKeyboard
+{
+    public class OSCKeyboard
+    {
+        private const int SOURCE_PORT = 8080;
+        private const string MESSAGE_FORMAT = "/osck/{0}";
+        private const string NOTE = "note";
+        private const string BEND = "bend";
+        private const string VIBRATO = "vibrato";
+
+        private IPEndPoint source;
+        private IPEndPoint dest;
+
+        public OSCKeyboard(IPAddress address, int port)
+        {
+            source = new IPEndPoint(IPAddress.Loopback, SOURCE_PORT);
+            dest = new IPEndPoint(address, port);
+        }
+
+        public void KeyDown(int note, int velocity)
+        {
+            int[] values = new int[2];
+            values[0] = note;
+            values[1] = velocity;
+
+            SendMessage(NOTE, values);
+        }
+
+        public void KeyUp(int note)
+        {
+            KeyDown(note, 0);
+        }
+
+        public void PitchBend(int value)
+        {
+            int[] values = new int[1];
+            values[0] = value;
+
+            SendMessage(BEND, values);
+        }
+
+        public void Vibrato(int value)
+        {
+            int[] values = new int[1];
+            values[0] = value;
+
+            SendMessage(VIBRATO, values);
+        }
+
+        private void SendMessage(string address, int[] values = null)
+        {
+            OscMessage msg = new OscMessage(source, string.Format(MESSAGE_FORMAT, address));
+
+            if (values != null)
+            {
+                foreach (object v in values)
+                {
+                    msg.Append(v);
+                }
+            }
+
+            msg.Send(dest);
+        }
+    }
+}
---
+++ OSCKeyboard/OSCKeyboard/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSCKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OSCKeyboard k = new OSCKeyboard(IPAddress.Parse("169.254.136.244"), 8080);
+          
+
+            int bend = 0;
+            int vib = 0;
+            DateTime last = DateTime.UtcNow;
+            bool noteOn = false;
+
+            while (true)
+            {
+                DateTime now = DateTime.UtcNow;
+                TimeSpan elapsed = now - last;
+
+                if (elapsed.Milliseconds > 500)
+                {
+                    last = now; 
+
+                    if (noteOn)
+                    {
+                        k.KeyUp(60);
+                    }
+                    else
+                    {
+                        k.KeyDown(60, 100);
+                    }
+
+                    noteOn = !noteOn;
+
+                    k.PitchBend(bend);
+                    bend = (bend + 10) % 127;
+                }
+
+                vib = (vib + 1) % 40;
+                Thread.Sleep(1);
+                //k.Vibrato(110 - vib);
+            }
+        }
+    }
+}
